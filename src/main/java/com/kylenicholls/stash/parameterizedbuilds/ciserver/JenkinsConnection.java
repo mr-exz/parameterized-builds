979b219ef2b24564c859fbb02930b3f1bbfb25a3
mr-exz <root@exzec.ru>
2025-02-14 10:44:57 +0300

    fixes for project jenkins server

--- src/main/java/com/kylenicholls/stash/parameterizedbuilds/ciserver/JenkinsConnection.java
+++ src/main/java/com/kylenicholls/stash/parameterizedbuilds/ciserver/JenkinsConnection.java
@@ -56,10 +56,16 @@ public class JenkinsConnection {
     public JenkinsResponse triggerJob(String projectKey, ApplicationUser user, Job job, 
                                       BitbucketVariables bitbucketVariables) {
         Server jenkinsServer;
-        logger.warn("PEPEGA HERE");
         if (job.getJenkinsServer() != null){
-            logger.warn(job.getJenkinsServer());
-            jenkinsServer = jenkins.getServerByAlias(job.getJenkinsServer());
+            // look for project jenkins server first
+            jenkinsServer = jenkins.getJenkinsServer(
+                                    job.getJenkinsServer(),
+                                    job.getJenkinsServer(), user
+            );
+            // then check on global level
+            if (jenkinsServer == null){
+                jenkinsServer = jenkins.getServerByAlias(job.getJenkinsServer());
+            }
         } else {
             // legacy behaviour
             Server projectServer = jenkins.getJenkinsServer(projectKey, null, user);
